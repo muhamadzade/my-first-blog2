b4687796e5c2e2f1b86681473d744587ff470442
muhamadzade <muhamadzade@gmail.com>
2021-08-29 10:36:12 +0430

    arhale css and view

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from tarix import views as vw
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('tarix/', vw.emrooz, name='emrooz'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
